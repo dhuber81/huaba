eb7e3c6cb39a972b9bab4e9db603de6ea8c8af03
dhuber81 <dhuber318@gmail.com>
2018-06-10 19:23:58 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Simple VC 3-Band Equalizer
+
+low band from 0Hz to 880Hz 
+mid band from 880Hz to 5000Hz 
+high band from 5000Hz to 24000Hz
+
+
---
+++ src/huaba.cpp
@@ -0,0 +1,12 @@
+#include "eq3.hpp"
+
+Plugin *plugin;
+
+void init(Plugin *p) {
+	plugin = p;
+	p->slug = TOSTRING(SLUG);
+	p->version = TOSTRING(VERSION);
+
+	// Add all Models defined throughout the plugin
+	p->addModel(modelEQ3);
+}
